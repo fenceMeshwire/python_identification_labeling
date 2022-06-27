08039c592c864814b78df1a4df115e74a9f013f0
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-06-27 21:48:46 +0200

    Create 01_create_barcode_ean.py

---
+++ 01_create_barcode_ean.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 01_create_barcode_code39.py
+
+import barcode
+from barcode.writer import ImageWriter
+import os
+
+# Change to specific output directory:
+p = 'C:\\Users\\name\\output'
+os.chdir(p)
+
+# The identifier must have a length of 12 digits, 13 is the check digit:
+# 1 2 3 4 5 6 7 8 9 10 11 12 (13)
+#                
+# Country code      (1-3)           
+# Company number    (4-9)  
+# Item number       (10-12)
+# Check digit       (13)
+
+identifier = '410525002200'   
+BCODE = barcode.get_barcode_class('ean')
+my_ean = BCODE(identifier, writer=ImageWriter())
+file = my_ean.save(identifier)
+
+print(file) # Print file name, file will be saved as PNG
