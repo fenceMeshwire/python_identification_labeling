b8202f677b7e5d60ed0eab039c8cdfebc053f404
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-06-29 23:05:19 +0200

    Create 04_create_pzn_barcode.py

---
+++ 04_create_pzn_barcode.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 04_create_pzn_barcode.py
+
+# The Pharmacy Central Number (PZN) is a nationally standardized 
+# identification code in Germany for pharmaceuticals, medical devices 
+# and other products commonly sold in pharmacies.
+
+# Dependencies
+import barcode
+from barcode.writer import ImageWriter
+import os
+
+# Change directory to specific output directory:
+p = 'C:\\Users\\name\\output'
+os.chdir(p)
+
+identifier = '8906763'
+BCODE = barcode.get_barcode_class('pzn')
+my_pzn = BCODE(identifier, writer=ImageWriter())
+file = my_pzn.save(identifier)
+
+print(file) # Print file name, file will be saved as PNG
